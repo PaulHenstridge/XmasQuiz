b6caf8c38f8f7d409000081306f89dd25548c889
Paul Henstridge <paulhenstridge@gmail.com>
2023-12-19 23:25:15 +0000

    added more questions

--- src/utils/questions.js
+++ src/utils/questions.js
@@ -4,6 +4,13 @@ import liverpool from "../assets/liverpool.png"
 import shark from "../assets/shark.png"
 import thistle from "../assets/thistle.png"
 import fash from "../assets/fash.png"
+import photo from "../assets/photo.png"
+import tigerShark from "../assets/tigerShark.png"
+import fly from "../assets/fly.png"
+import butterfly from "../assets/butterfly.png"
+import everything from "../assets/everything.png"
+import QR from "../assets/QR.png"
+
 
 export const questionsArray = [
     { id: 'q1', question: 'What is 2+2?', answer: '4' },
@@ -13,7 +20,14 @@ export const questionsArray = [
     { id: 'q5', question: 'In the song "Baby Shark", how many times do they sing the word "doo" ?', answer: '162', img: shark },
     { id: 'q6', question: 'In "Flower of Scotland", they stood against him.  Against who?', answer: 'proud edwards army', img: thistle },
     { id: 'q7', question: 'What kind of fish is this?', answer: 'haddock', img: fash },
-    
+    { id: 'q8', question: 'A man looks at a photograph and exclaims, “Brothers and sisters I have none, but that man’s father is my father’s son!” Who is the photograph of?', answer: 'his son', img: photo },
+    { id: 'q9', question: 'What animal is this?', answer: 'tiger shark', img: tigerShark },
+    { id: 'q10', question: 'What animal is this?', answer: 'horsefly', img: fly },
+    { id: 'q11', question: 'What animal is this?', answer: 'butterfly', img: butterfly },
+    { id: 'q12', question: 'What is at the end of everything?', answer: 'g', img: everything },
+    { id: 'q13', question: 'Find the mysterious QR of destiny to know the answer!  Clue: Look where you find lots of keys that dont fit any locks!', answer: '###', img: QR  },
+
+
 
     // ... more questions
 ];
