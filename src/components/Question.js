e0d8e8da48d87ef7b29aa018d7a334d7561f0fb9
Paul Henstridge <paulhenstridge@gmail.com>
2023-12-19 11:16:11 +0000

    conditional formatting on correct answer

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,20 +1,41 @@
 import { useState } from "react";
+import styled from 'styled-components';
 
-const Question = ({question, correctAnswer}) => {
+const AnswerInput = styled.input`
+    /* Default input styles */
+    &:disabled {
+        background-color: #e0e0e0;
+        color: #9e9e9e;
+    }
+`;
+
+const QuestionBox = styled.div`
+    /* Default container styles */
+    background-color: ${props => props.isAnswered ? '#d4edda' : 'white'}; // Change background if answered
+`;
+
+const Question = ({question, correctAnswer, onCorrectAnswer}) => {
 
     const [userInput, setUserInput] = useState('');
+    const [isAnswered, setIsAnswered] = useState(false);
 
     const handleChange = (event) => {
         setUserInput(event.target.value);
+        if (event.target.value === correctAnswer && !isAnswered) {
+            setIsAnswered(true)
+            onCorrectAnswer(); // Notify parent component
+        }
     }
-    return ( <>
-    <h3>{question}</h3>
-          <input 
-          placeholder="answer here"
-          value = {userInput}
-          onChange={handleChange}
-          ></input>
-    </> );
+    return ( 
+    <QuestionBox isAnswered={isAnswered}>
+        <h3>{question}</h3>
+          <AnswerInput 
+            placeholder="answer here"
+            value = {userInput}
+            onChange={handleChange}
+            disabled={isAnswered}
+         />
+    </QuestionBox> );
 }
  
 export default Question;
--- src/components/QuestionContainer.js
+++ src/components/QuestionContainer.js
@@ -1,13 +1,28 @@
+import { useState } from "react";
+
 import Question from "./Question";
 import { questionsArray } from "../utils/questions";
 
 const QuestionContainer = () => {
+    const [answeredQuestions, setAnsweredQuestions] = useState({});
+
+    const handleCorrectAnswer = (questionId) => {
+        setAnsweredQuestions({
+            ...answeredQuestions,
+            [questionId]: true
+        });
+    };
+
+
+
     return ( <>
+    <h1>{Object.keys(answeredQuestions)}</h1>
     {questionsArray.map(q => {
         return <Question 
         key={q.id}
         question={q.question} 
         correctAnswer={q.answer} 
+        onCorrectAnswer={() => handleCorrectAnswer(q.id)}
         />
     })}
     </> );
