434646beda470417607125486f42ef76eb9c8506
Paul Henstridge <paulhenstridge@gmail.com>
2023-12-19 11:50:21 +0000

    parent managing state, returning win condition

--- src/components/QuestionContainer.js
+++ src/components/QuestionContainer.js
@@ -1,30 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Question from "./Question";
 import { questionsArray } from "../utils/questions";
 
 const QuestionContainer = () => {
     const [answeredQuestions, setAnsweredQuestions] = useState({});
+    const [gameCompleted, setGameCompleted] = useState(false);
 
     const handleCorrectAnswer = (questionId) => {
-        setAnsweredQuestions({
-            ...answeredQuestions,
+        setAnsweredQuestions(prevState => ({
+            ...prevState,
             [questionId]: true
-        });
+        }));
     };
 
+    useEffect(() => {
+        const answeredCount = Object.keys(answeredQuestions).length;
+        if (answeredCount === questionsArray.length) {
+            setGameCompleted(true);
+        }
+    }, [answeredQuestions]);
 
 
     return ( <>
     <h1>{Object.keys(answeredQuestions)}</h1>
-    {questionsArray.map(q => {
-        return <Question 
-        key={q.id}
-        question={q.question} 
-        correctAnswer={q.answer} 
-        onCorrectAnswer={() => handleCorrectAnswer(q.id)}
+    {questionsArray.map(q => (
+        <Question 
+            key={q.id}
+            question={q.question} 
+            correctAnswer={q.answer} 
+            onCorrectAnswer={() => handleCorrectAnswer(q.id)}
         />
-    })}
+    ))}
+    
+    {gameCompleted && <div> You completed the quiz!</div>}
+
     </> );
 }
  
